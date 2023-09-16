7ff11c80f4b224d4227819e5df8e3e701a4e170c
olili27 <timmolili27@gmail.com>
2023-09-16 14:43:17 +0300

    array manipulation

--- script.js
+++ script.js
@@ -4,31 +4,107 @@ const data = [
 ];
 
 // 1. Print Developers
-function printDeveloper() {}
+function printDeveloper() {
+  const developers = data.filter(
+    (element) => element.profession == "developer"
+  );
+  console.log(developers);
+}
 
 // 2. Add Data
-function addData() {}
+function addData() {
+  const newData = {
+    name: "timothy",
+    age: 24,
+    profession: "developer",
+  };
+
+  data.push(newData);
+  console.log(data);
+}
 
 // 3. Remove Admins
-function removeAdmin() {}
+function removeAdmin() {
+  const admins = data.filter((element) => element.profession == "admin");
+
+  admins.forEach((admin) => {
+    let index = data.indexOf(admin);
+    data.splice(index, 1);
+  });
+
+  console.log(data);
+}
 
 // 4. Concatenate Array
-function concatenateArray() {}
+function concatenateArray() {
+  const arr = [
+    {name: "tom", age: 18, profession: "developer" },
+    {name: "tony", age: 20, profession: "designer" },
+    {name: "kane", age: 19, profession: "admin" },
+  ];
+
+  arr.push(...data);
+  console.log(arr);
+}
 
 // 5. Average Age
-function averageAge() {}
+function averageAge() {
+  let sum = 0;
+
+  data.forEach(element => {
+    sum += element.age;
+  });
+
+  let averageAge = sum / data.length;
+  console.log(averageAge);
+}
 
 // 6. Age Check
-function checkAgeAbove25() {}
+function checkAgeAbove25() {
+  let value = data.find(element => element.age > 25)
+
+  if(value != undefined) console.log("yes");
+  else console.log("no");
+}
 
 // 7. Unique Professions
-function uniqueProfessions() {}
+function uniqueProfessions() {
+  let counter = {};
+
+  data.forEach(element => {
+    if(counter[element.profession] == undefined) {
+      counter[element.profession] = 1;
+    } 
+    else counter[element.profession]++;
+  });
+
+   let distinct = [];
+   for (const key in counter) {
+    
+     if (counter[key] == 1) {
+      distinct.push(key);
+    }
+   }
+
+   console.log(distinct);
+}
 
 // 8. Sort by Age
-function sortByAge() {}
+function sortByAge() {
+  data.sort((a, b)=> a.age - b.age);
+  console.log(data);
+}
 
 // 9. Update Profession
-function updateJohnsProfession() {}
+function updateJohnsProfession() {
+    const john = data.find(el => el.name == "john");
+    john.profession = "manager";
+
+    console.log(data);
+}
 
 // 10. Profession Count
-function getTotalProfessions() {}
+function getTotalProfessions() {
+  const arr = data.filter(el => el.profession == "admin" || el.profession == "developer");
+  console.log(arr.length);
+}
